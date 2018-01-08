ed22e09fb0beabad04eeaa25e651758a2dccf7d9
Lombardi <lombardi2008@gmail.com>
2018-01-08 21:25:36 -0200

    testing rest

--- src/main/java/com/udmc/app/UdemyCursoMcApplication.java
+++ src/main/java/com/udmc/app/UdemyCursoMcApplication.java
@@ -2,8 +2,10 @@ package com.udmc.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages= {"com.udmc.resources"})
 public class UdemyCursoMcApplication {
 
 	public static void main(String[] args) {
