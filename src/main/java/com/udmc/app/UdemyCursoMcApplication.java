8a5018570c9282482e0b5d81df10456e42fc6bc1
Lombardi <lombardi2008@gmail.com>
2018-01-08 20:58:01 -0200

    first commit

---
+++ src/main/java/com/udmc/app/UdemyCursoMcApplication.java
@@ -0,0 +1,13 @@
+package com.udmc.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyCursoMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyCursoMcApplication.class, args);
+	}
+	
+}
