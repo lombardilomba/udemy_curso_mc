8caa8ad3906491b7cafb457847e676769a7e73d4
Lombardi <lombardi2008@gmail.com>
2018-01-08 21:34:31 -0200

    testing categoria

--- src/main/java/com/udmc/resources/CategoriaResource.java
+++ src/main/java/com/udmc/resources/CategoriaResource.java
@@ -1,16 +1,24 @@
 package com.udmc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.udmc.model.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST test";
+	public List<Categoria> listar() {
+		List<Categoria> categoriaList = new ArrayList<Categoria>();
+		categoriaList.add(new Categoria(1L, "INFORMATICA"));
+		categoriaList.add(new Categoria(2L, "ESCRITORIO"));
+		return categoriaList;
 	}
 	
 }
